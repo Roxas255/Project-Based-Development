4e07c1a0b1451ab595003cd87beef89f7a1e5313
RaphaelC720 <rdc1030@gmail.com>
2026-02-18 18:17:20 -0500

    mainmenu setup
    hi

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+public class MainMenuScript : MonoBehaviour
+{
+    //public gameobject Settings_Panel;
+    public void OpenSettingsPanel()
+    {
+        //Settings_Panel.SetActive(true);
+    }
+}
