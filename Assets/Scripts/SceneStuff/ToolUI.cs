8115029c0ab4aba36038a80763def479209cba2b
RaphaelC720 <rdc1030@gmail.com>
2026-03-01 00:08:30 -0500

    Added tool buttons
    buttons for tools were added and a temporary house holder.

---
+++ Assets/Scripts/SceneStuff/ToolUI.cs
@@ -0,0 +1,8 @@
+using TMPro;
+using UnityEngine;
+
+public class ToolUI : MonoBehaviour
+{
+    [SerializeField] private GameObject toolUIThing;
+    [SerializeField] private TextMeshProUGUI toolUIText;
+}
