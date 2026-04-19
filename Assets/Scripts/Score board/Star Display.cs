816c3742af7d7308ac00e30b90e9effbc546b328
Roxas255 <aemartinez1224@icloud.com>
2026-04-19 01:10:55 -0400

    Stars and Stamps

--- Assets/Scripts/Score board/Star Display.cs
+++ Assets/Scripts/Score board/Star Display.cs
@@ -7,28 +7,32 @@ public class StarDisplay : MonoBehaviour
     public Image star1;
     public Image star2;
     public Image star3;
-
+    public GameObject stamp;
     public void SetStars(int score)
     {
-        // Turn all off first
+        // Turn all off stars first
         star1.enabled = false;
         star2.enabled = false;
         star3.enabled = false;
 
+        // Hides stamp first
+        stamp.SetActive(false);
+
         if (score >= 15 && score <= 29)
         {
             star1.enabled = true;
+            stamp.SetActive(true); 
         }
         else if (score >= 30 && score <= 39)
         {
             star1.enabled = true;
             star2.enabled = true;
+            stamp.SetActive(true); 
         }
         else if (score >= 40)
         {
             star1.enabled = true;
             star2.enabled = true;
-            star3.enabled = true;
         }
     }
 }
