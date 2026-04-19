d9b2c98b95bea63b8b197ab1c936c18fa2063460
RaphaelC720 <rdc1030@gmail.com>
2026-04-19 03:15:38 -0400

    Fixed bugs in tutorial and made sure it worked
    Tutorial main stuff is done.

--- Assets/Scripts/SceneStuff/Random Stuff/PopupTextScript.cs
+++ Assets/Scripts/SceneStuff/Random Stuff/PopupTextScript.cs
@@ -13,6 +13,8 @@ public class PopupTextScript : MonoBehaviour
     public GameObject WindowQuestionPanel;
     public GameObject WindowInfoPanel;
     public TextMeshProUGUI WindowQuestionText;
+    //public GameObject PipeInfo;
+    public TextMeshProUGUI PipeQuestionText;
 
     public GameObject AfterWindowGamePopUp;
     void Start()
@@ -79,12 +81,19 @@ public class PopupTextScript : MonoBehaviour
     }
     public IEnumerator ChangeQuestionText(string newText)
     {
-        WindowInfoPanel.SetActive(true);
         WindowQuestionText.text = newText;
         yield return new WaitForSeconds(2f);
         WindowQuestionText.text = ("Where is the defect located?");
     }   
 
-
-    
+    public void PipeQuestionWrong()
+    {
+        StartCoroutine(ChangeQuestionText2("Incorrect. Try again."));
+    }
+    public IEnumerator ChangeQuestionText2(string newText)
+    {
+        PipeQuestionText.text = newText;
+        yield return new WaitForSeconds(2f);
+        PipeQuestionText.text = ("Where is the defect located?");
+    }   
 }
